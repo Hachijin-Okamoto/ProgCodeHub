9b519d4e48d3173227ed4d1c72054ebc38645368
Hachijin-Okamoto <ce255001@meiji.ac.jp>
2025-12-12 15:53:41 +0900

    環境構築1

---
+++ .eslint.config.mjs
@@ -0,0 +1,78 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import unusedImports from "eslint-plugin-unused-imports";
+
+export default defineConfig([
+  {
+    ignores: ["src/modules/antlr/**"],
+  },
+  
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        {
+          "selector": "class",
+          "format": ["PascalCase"],
+        },
+        {
+          "selector": "typeAlias",
+          "format": ["PascalCase"],
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-function-return-type": [
+        "error",
+        {
+          allowExpressions: false,
+          allowTypedFunctionExpressions: true,
+          allowHigherOrderFunctions: true,
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": "off",
+        "unused-imports/no-unused-imports": "error",
+        "unused-imports/no-unused-vars": [
+            "warn",
+            {
+                "vars": "all",
+                "varsIgnorePattern": "^_",
+                "args": "after-used",
+                "argsIgnorePattern": "^_",
+            },
+        ],
+      "@typescript-eslint/explicit-module-boundary-types": "warn",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/typedef": [
+        "error",
+        {
+          "arrayDestructuring": false,
+          "objectDestructuring": false,
+          "variableDeclaration": true,
+          "variableDeclarationIgnoreFunction": true,
+          "parameter": true,
+          "propertyDeclaration": true,
+          "memberVariableDeclaration": false
+        },
+      ],
+    },
+  },
+]);
