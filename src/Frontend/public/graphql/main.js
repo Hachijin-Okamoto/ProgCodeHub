c53ebc0e80e29f4a1d83130064ddb38e25946ad7
Hachijin-Okamoto <ce255001@meiji.ac.jp>
2025-12-17 11:07:28 +0900

    graphqlのフロント仮実装

---
+++ src/Frontend/public/graphql/main.js
@@ -0,0 +1,15 @@
+const GRAPHQL_ENDPOINT = "http://localhost:8000/graphql";
+
+async function gql(query, variables = {}) {
+  const res = await fetch(GRAPHQL_ENDPOINT, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ query, variables }),
+  });
+  const json = await res.json();
+  return json.data;
+}
+
+function getQueryParam(name) {
+  return new URLSearchParams(location.search).get(name);
+}
